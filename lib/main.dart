899f5a8e038613bfb96c19c95981fa5017a4c69d
kimdahee7 <kikidahee7@naver.com>
2022-10-05 01:52:25 +0900

    [feat] FavoriteWidget 기능 추가

--- lib/main.dart
+++ lib/main.dart
@@ -213,11 +213,7 @@ Widget _titleSection() {
             ],
           ),
         ),
-        Icon(
-          Icons.star,
-          color: Colors.red[500],
-        ),
-        const Text('41'),
+        FavoriteWidget(),
       ],
     ),
   );
@@ -245,6 +241,55 @@ Column _buildButtonColum(Color color, IconData icon, String label) {
     ],
   );
 }
+
+//별모양 아이콘 누르기
+class FavoriteWidget extends StatefulWidget {
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text('$_favoriteCount'),
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+}
+
+
+
 
 
 
