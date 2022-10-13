81bb0e90984ef2d1aa3d363d3c924efea73bff85
kimdahee7 <kikidahee7@naver.com>
2022-10-13 23:18:42 +0900

    [feat] TabBarScreen 생성

---
+++ lib/tabbar_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class TabBarScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: const TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.directions_car)),
+              Tab(icon: Icon(Icons.directions_transit)),
+              Tab(icon: Icon(Icons.directions_bike)),
+            ],
+          ),
+          title: const Text('Tabs Demo'),
+        ),
+        body: const TabBarView(
+          children: [
+            Icon(Icons.directions_car),
+            Icon(Icons.directions_transit),
+            Icon(Icons.directions_bike),
+          ],
+        ),
+      ),
+    );
+  }
+}
