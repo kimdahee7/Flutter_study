4d283092352baf139fcf1ceb75dd3dbf7e3e0b4a
Kimdahee <kikidahee7@duksung.ac.kr>
2022-10-18 20:01:43 +0900

    [feat] 첫번째 Tab에 MyCustomForm 추가

--- lib/tabbar_screen.dart
+++ lib/tabbar_screen.dart
@@ -20,12 +20,54 @@ class TabBarScreen extends StatelessWidget {
         ),
         body: const TabBarView(
           children: [
-            Icon(Icons.directions_car),
+            MyCustemForm(),
             Icon(Icons.directions_transit),
             Icon(Icons.directions_bike),
           ],
         ),
       ),
     );
+  }
+}
+
+class MyCustemForm extends StatefulWidget {
+  const MyCustemForm({super.key});
+
+  @override
+  MyCustomFormState createState() => MyCustomFormState();
+}
+
+class MyCustomFormState extends State<MyCustemForm> {
+  final _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            TextFormField(
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please enter some text';
+                } else {
+                  return null;
+                }
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Processing Data')));
+                  }
+                },
+                child: const Text('Submit'),
+              ),
+            )
+          ],
+        ));
   }
 }
